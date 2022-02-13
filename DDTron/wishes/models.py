46ddaeb14bba62c3fbee866f6f7abd8b1a21cec2
Krzysztof Baranowski <krzybaranowski@gmail.com>
2022-02-13 19:43:14 +0100

    model change

--- DDTron/wishes/models.py
+++ DDTron/wishes/models.py
@@ -1,15 +1,15 @@
 from pickle import NONE
 from django.db import models
 from django.forms import CharField
-#from people.models import Person
-#from events.models import Event
+from people.models import Person
+from events.models import Event
 
 # Create your models here.
 
 
 class Wish(models.Model):
     text = CharField(max_length=2000)
-    #event = models.ForeignKey(Event, on_delete=models.DO_NOTHING, default=NONE)
-    #owner = models.ForeignKey(Person, on_delete=models.DO_NOTHING, default=NONE)
+    event = CharField(max_length=100)              #models.ForeignKey(Event, on_delete=models.DO_NOTHING, default=0)
+    owner = CharField(max_length=100)             #models.ForeignKey(Person, on_delete=models.DO_NOTHING, default=0)
 
 
